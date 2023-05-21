221be82a4ae79907f533b3847a6d6c61dcaf9358
K0sPr0 <Kos.Pro531@gmail.com>
2023-05-21 17:33:43 +0200

    please work

---
+++ Assets/Collision1.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Collision1 : MonoBehaviour
+{
+    [HideInInspector]
+    public float size;
+    [HideInInspector]
+    float wallSize;
+    public Rigidbody2D rb;
+    private SpriteRenderer renderer;
+    private SpriteRenderer wallRenderer;
+    public GameObject wall;
+    public GameObject obj2;
+    public Collision2 other;
+    public TMP_Text bounceText;
+    public TMP_Text object1Text;
+    public TMP_Text object2Text;
+    public TMP_Text noMoreBounces;
+    public float maxVel;
+    public float velocity;
+    public float slowMoVelocity;
+    // [HideInInspector]
+    public float inVelocity;
+    public float mass;
+    public float seconds;
+    public float timeSinceLastBounce;
+    public float bounces;
+    bool collision;
+    bool willBounce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // InvokeRepeating("BouncesPerSecond", .01f, 1.0f);
+
+        renderer = GetComponent<SpriteRenderer>();
+        size = renderer.bounds.size.x;
+
+        wallRenderer = wall.GetComponent<SpriteRenderer>();
+        wallSize = wallRenderer.bounds.size.x;
+
+        velocity = 0;
+        timeSinceLastBounce = 1;
+        bounces = 0;
+        collision = false;
+        willBounce = true;
+
+        rb.freezeRotation = true;
+
+        other.xFreezePos=(wall.transform.position.x + wallSize/2)+size+other.size/1.75f;
+    }
+     
+    private void OnTriggerEnter2D(Collider2D oth) 
+    {
+        collision=true;
+        if (oth.gameObject.tag != "Wall"){
+            velocity = other.inVelocity * ((2 * other.mass) / (other.mass + mass)) + inVelocity * ((mass - other.mass) / (other.mass + mass));
+            other.velocity = other.inVelocity * ((other.mass - mass) / (other.mass + mass)) + inVelocity *((2 * mass)/(other.mass + mass));
+        }
+        else 
+        {
+            velocity = -velocity;
+        }
+        Debug.Log("collide");
+
+        timeSinceLastBounce=seconds;
+        seconds=0;
+        bounces+=1;
+        collision=false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        seconds += Time.deltaTime*(timeSinceLastBounce*10);
+        slowMoVelocity = velocity;
+        
+        bounceText.text = "Bounces:"+ bounces.ToString();
+
+        if (velocity<0)
+        {
+            object2Text.text = "m2:" + mass.ToString() + "\nv2:" + velocity.ToString();
+        }
+        else
+        {
+            object2Text.text = "m2:" + mass.ToString() + "\nv2: " + velocity.ToString();
+        }
+        
+        if (other.velocity<0)
+        {
+            object1Text.text = "m1:100^" + other.n.ToString() + "\nv1:" + other.velocity.ToString();
+        }
+        else
+        {
+            object1Text.text = "m1:100^" + other.n.ToString() + "\nv1: " + other.velocity.ToString();
+        }
+
+        if (slowMoVelocity>maxVel)
+        {
+            slowMoVelocity=maxVel;
+        }
+        else if (slowMoVelocity<-maxVel)
+        {
+            slowMoVelocity=-maxVel;
+        }
+
+        if (other.velocity > Mathf.Abs(velocity) && velocity>=0)
+        {
+            willBounce = false;
+        }
+        noMoreBounces.enabled = !willBounce;
+
+        if (!collision)
+        {
+            inVelocity = velocity;
+            other.inVelocity = other.velocity;
+        }
+        rb.velocity = new Vector2(slowMoVelocity, 0);
+    }
+}
---
+++ Assets/Collision2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision2 : MonoBehaviour
+{
+    [HideInInspector]
+    public float size;
+    private SpriteRenderer renderer;
+    public Rigidbody2D rb;
+    public float velocity;
+    // [HideInInspector]
+    public float inVelocity;
+    public float n;
+    public float mass;
+    public float checkVel;
+    // [HideInInspector]
+    public float xFreezePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+        size = renderer.bounds.size.x;
+
+        velocity = -1f;
+
+        rb.freezeRotation = true;
+        mass = Mathf.Pow(100,(n));
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(velocity, 0);
+
+        checkVel =Mathf.Pow(0.9999998f,Mathf.Pow(10,7-n));
+
+        if (gameObject.transform.position.x <=xFreezePos && velocity<checkVel) {
+            rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+        else 
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+        }
+    }
+}
